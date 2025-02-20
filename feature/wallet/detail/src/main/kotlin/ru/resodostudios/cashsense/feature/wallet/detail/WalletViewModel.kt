4c8d224576cbafd0acc230be80ab77c7bfd0dc8d
Nikita Bulavin <nik.bulavin.005@gmail.com>
2025-02-20 11:36:11 +0300

    Group transaction lists by days instead of day of week for month and all dates in filters

--- feature/wallet/detail/src/main/kotlin/ru/resodostudios/cashsense/feature/wallet/detail/WalletViewModel.kt
+++ feature/wallet/detail/src/main/kotlin/ru/resodostudios/cashsense/feature/wallet/detail/WalletViewModel.kt
@@ -80,8 +80,8 @@ class WalletViewModel @Inject constructor(
                 val zonedDateTime = it.transaction.timestamp.getZonedDateTime()
                 when (transactionFilter.dateType) {
                     YEAR -> zonedDateTime.monthNumber
-                    MONTH -> zonedDateTime.dayOfMonth
-                    ALL, WEEK -> zonedDateTime.dayOfWeek.value
+                    ALL, MONTH -> zonedDateTime.dayOfMonth
+                    WEEK -> zonedDateTime.dayOfWeek.value
                 }
             }
             .mapValues { (_, transactions) ->
@@ -164,7 +164,12 @@ class WalletViewModel @Inject constructor(
             it.copy(financeType = financeType)
         }
         if (financeType == NOT_SET) {
-            transactionFilterState.update { it.copy(selectedCategories = emptySet()) }
+            transactionFilterState.update {
+                it.copy(
+                    selectedCategories = emptySet(),
+
+                )
+            }
         }
     }
 
