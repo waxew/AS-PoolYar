9fbf5717c7b7a2e1efafdff77d368870b6df3926
Nikita Bulavin <nik.bulavin.005@gmail.com>
2025-03-05 23:21:11 +0300

    Refactor `ReminderBroadcastReceiver` to use the default dispatcher

--- core/data/src/main/kotlin/ru/resodostudios/cashsense/core/data/util/ReminderBroadcastReceiver.kt
+++ core/data/src/main/kotlin/ru/resodostudios/cashsense/core/data/util/ReminderBroadcastReceiver.kt
@@ -4,14 +4,11 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withTimeoutOrNull
 import ru.resodostudios.cashsense.core.data.repository.SubscriptionsRepository
-import ru.resodostudios.cashsense.core.network.CsDispatchers.IO
-import ru.resodostudios.cashsense.core.network.Dispatcher
 import ru.resodostudios.cashsense.core.network.di.ApplicationScope
 import ru.resodostudios.cashsense.core.notifications.Notifier
 import javax.inject.Inject
@@ -31,14 +28,11 @@ internal class ReminderBroadcastReceiver : BroadcastReceiver() {
     @Inject @ApplicationScope
     lateinit var appScope: CoroutineScope
 
-    @Inject @Dispatcher(IO)
-    lateinit var ioDispatcher: CoroutineDispatcher
-
     override fun onReceive(context: Context, intent: Intent) {
 
         val reminderId = intent.getIntExtra(EXTRA_REMINDER_ID, 0)
 
-        appScope.launch(ioDispatcher) {
+        appScope.launch {
             withTimeoutOrNull(4500L)  {
                 if (reminderId != 0) {
                     findSubscriptionAndPostNotification(reminderId)
