10ed1deb26b8497206a817ce6789b4c0c7715e1c
Nikita Bulavin <nik.bulavin.005@gmail.com>
2025-04-09 23:49:31 +0300

    Update `CsFloatingActionButton` to use `MediumFloatingActionButton`

--- core/designsystem/src/main/kotlin/ru/resodostudios/cashsense/core/designsystem/component/FloatingActionButton.kt
+++ core/designsystem/src/main/kotlin/ru/resodostudios/cashsense/core/designsystem/component/FloatingActionButton.kt
@@ -1,13 +1,17 @@
 package ru.resodostudios.cashsense.core.designsystem.component
 
 import androidx.annotation.StringRes
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MediumFloatingActionButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun CsFloatingActionButton(
     @StringRes contentDescriptionRes: Int,
@@ -15,13 +19,14 @@ fun CsFloatingActionButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = {},
 ) {
-    FloatingActionButton(
+    MediumFloatingActionButton(
         onClick = onClick,
         modifier = modifier,
     ) {
         Icon(
             imageVector = icon,
             contentDescription = stringResource(contentDescriptionRes),
+            modifier = Modifier.size(30.dp),
         )
     }
 }
