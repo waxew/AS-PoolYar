2497353670bb0ec7851ff6bb7819901210461751
Nikita Bulavin <nik.bulavin.005@gmail.com>
2025-04-18 20:37:11 +0300

    Implement data export and import functionality in `UserDataRepository`

--- core/data/src/main/kotlin/ru/resodostudios/cashsense/core/data/repository/offline/OfflineUserDataRepository.kt
+++ core/data/src/main/kotlin/ru/resodostudios/cashsense/core/data/repository/offline/OfflineUserDataRepository.kt
@@ -1,8 +1,10 @@
 package ru.resodostudios.cashsense.core.data.repository.offline
 
+import android.net.Uri
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import ru.resodostudios.cashsense.core.data.repository.UserDataRepository
+import ru.resodostudios.cashsense.core.database.util.DatabaseTransferManager
 import ru.resodostudios.cashsense.core.datastore.CsPreferencesDataSource
 import ru.resodostudios.cashsense.core.model.data.DarkThemeConfig
 import ru.resodostudios.cashsense.core.model.data.UserData
@@ -12,6 +14,7 @@ import javax.inject.Inject
 internal class OfflineUserDataRepository @Inject constructor(
     private val csPreferencesDataSource: CsPreferencesDataSource,
     private val shortcutManager: ShortcutManager,
+    private val databaseTransferManager: DatabaseTransferManager,
 ) : UserDataRepository {
 
     override val userData: Flow<UserData> = csPreferencesDataSource.userData
@@ -34,4 +37,10 @@ internal class OfflineUserDataRepository @Inject constructor(
 
     override suspend fun setCurrency(currency: String) =
         csPreferencesDataSource.setCurrency(currency)
+
+    override fun exportData(backupFileUri: Uri) =
+        databaseTransferManager.export(backupFileUri)
+
+    override fun importData(backupFileUri: Uri, restart: Boolean) =
+        databaseTransferManager.import(backupFileUri, restart)
 }
