24780ceed54cbbd9a969053dfc221445c6f115d3
Nikita Bulavin <nik.bulavin.005@gmail.com>
2025-05-16 19:45:39 +0300

    Add thumb icon to `CsSwitch`

--- core/designsystem/src/main/kotlin/ru/resodostudios/cashsense/core/designsystem/component/Switch.kt
+++ core/designsystem/src/main/kotlin/ru/resodostudios/cashsense/core/designsystem/component/Switch.kt
@@ -1,10 +1,16 @@
 package ru.resodostudios.cashsense.core.designsystem.component
 
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
+import ru.resodostudios.cashsense.core.designsystem.icon.CsIcons
+import ru.resodostudios.cashsense.core.designsystem.icon.outlined.Check
+import ru.resodostudios.cashsense.core.designsystem.icon.outlined.Close
 
 @Composable
 fun CsSwitch(
@@ -20,6 +26,21 @@ fun CsSwitch(
                 if (isChecked) HapticFeedbackType.ToggleOn else HapticFeedbackType.ToggleOff
             )
             onCheckedChange(isChecked)
+        },
+        thumbContent = {
+            if (checked) {
+                Icon(
+                    imageVector = CsIcons.Outlined.Check,
+                    contentDescription = null,
+                    modifier = Modifier.size(SwitchDefaults.IconSize),
+                )
+            } else {
+                Icon(
+                    imageVector = CsIcons.Outlined.Close,
+                    contentDescription = null,
+                    modifier = Modifier.size(SwitchDefaults.IconSize),
+                )
+            }
         },
         modifier = modifier,
     )
