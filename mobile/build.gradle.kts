6b7811cbda50cbbe124d19e40e6fd5c3c9f2dcd7
Nikita Bulavin <nik.bulavin.005@gmail.com>
2025-06-05 18:36:22 +0300

    Add LeakCanary to debug builds

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -91,6 +91,7 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
 
     debugImplementation(libs.androidx.compose.ui.testManifest)
+    debugImplementation(libs.leakcanary.android)
 
     baselineProfile(projects.baselineprofile)
 }
