2bd3412229f1400e7826692d8540233938279c84
Nikita Bulavin <nikbulavin@proton.me>
2025-08-02 19:38:30 +0300

    Add Ktor client dependencies

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -16,6 +16,15 @@ dependencies {
     api(projects.core.model)
 
     implementation(libs.kotlinx.serialization.json)
+    implementation(platform(libs.ktor.bom))
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.resources)
+    implementation(libs.ktor.serialization.kotlinx.json)
+
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.okhttp.logging)
     implementation(libs.retrofit.core)
     implementation(libs.retrofit.kotlin.serialization)
