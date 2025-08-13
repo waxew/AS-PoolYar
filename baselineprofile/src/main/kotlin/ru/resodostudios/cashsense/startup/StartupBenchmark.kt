b8d92f8b7302b77bcab092eb836558afe3237b7c
Nikita Bulavin <nikbulavin@proton.me>
2025-08-13 23:12:48 +0300

    Add `StartupBenchmark`

---
+++ baselineprofile/src/main/kotlin/ru/resodostudios/cashsense/startup/StartupBenchmark.kt
@@ -0,0 +1,56 @@
+package ru.resodostudios.cashsense.startup
+
+import androidx.benchmark.macro.BaselineProfileMode.Disable
+import androidx.benchmark.macro.BaselineProfileMode.Require
+import androidx.benchmark.macro.CompilationMode
+import androidx.benchmark.macro.StartupMode.COLD
+import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import ru.resodostudios.cashsense.BaselineProfileMetrics
+import ru.resodostudios.cashsense.PACKAGE_NAME
+import ru.resodostudios.cashsense.home.homeWaitForContent
+
+/**
+ * Enables app startups from various states of baseline profile or [CompilationMode]s.
+ * Run this benchmark from Studio to see startup measurements, and captured system traces
+ * for investigating your app's performance from a cold state.
+ */
+@RunWith(AndroidJUnit4ClassRunner::class)
+class StartupBenchmark {
+
+    @get:Rule
+    val benchmarkRule = MacrobenchmarkRule()
+
+    @Test
+    fun startupWithoutPreCompilation() = startup(CompilationMode.None())
+
+    @Test
+    fun startupWithPartialCompilationAndDisabledBaselineProfile() = startup(
+        CompilationMode.Partial(baselineProfileMode = Disable, warmupIterations = 1),
+    )
+
+    @Test
+    fun startupPrecompiledWithBaselineProfile() =
+        startup(CompilationMode.Partial(baselineProfileMode = Require))
+
+    @Test
+    fun startupFullyPrecompiled() = startup(CompilationMode.Full())
+
+    private fun startup(compilationMode: CompilationMode) = benchmarkRule.measureRepeated(
+        packageName = PACKAGE_NAME,
+        metrics = BaselineProfileMetrics.allMetrics,
+        compilationMode = compilationMode,
+        // More iterations result in higher statistical significance.
+        iterations = 20,
+        startupMode = COLD,
+        setupBlock = {
+            pressHome()
+        },
+    ) {
+        startActivityAndWait()
+        homeWaitForContent()
+    }
+}
