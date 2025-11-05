c349057a4c36cfebb3106afca3d03011e8faec6d
Nikita Bulavin <nikbulavin@proton.me>
2025-11-05 12:11:14 +0300

    Add `dropShadow` modifier for selected state

--- feature/home/src/main/kotlin/ru/resodostudios/cashsense/feature/home/WalletCard.kt
+++ feature/home/src/main/kotlin/ru/resodostudios/cashsense/feature/home/WalletCard.kt
@@ -8,7 +8,6 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
@@ -22,7 +21,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ButtonGroup
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
@@ -35,9 +33,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.dropShadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.shadow.Shadow
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.LookaheadScope
 import androidx.compose.ui.res.stringResource
@@ -72,21 +71,25 @@ fun WalletCard(
     onTransferClick: (String) -> Unit,
     modifier: Modifier = Modifier,
     selected: Boolean = false,
+    shape: Shape = MaterialTheme.shapes.extraLarge,
 ) {
-    val border = if (selected) {
-        BorderStroke(
-            width = 2.dp,
-            brush = SolidColor(MaterialTheme.colorScheme.outlineVariant),
-        )
-    } else {
-        CardDefaults.outlinedCardBorder()
-    }
-
     OutlinedCard(
         onClick = { onWalletClick(userWallet.id) },
-        shape = RoundedCornerShape(24.dp),
-        border = border,
-        modifier = modifier,
+        shape = shape,
+        modifier = modifier.then(
+            if (selected) {
+                Modifier.dropShadow(
+                    shape = shape,
+                    shadow = Shadow(
+                        radius = 8.dp,
+                        spread = 2.dp,
+                        color = MaterialTheme.colorScheme.inverseSurface.copy(0.2f),
+                    )
+                )
+            } else {
+                Modifier
+            }
+        ),
     ) {
         Column(
             verticalArrangement = Arrangement.spacedBy(4.dp),
@@ -329,6 +332,34 @@ fun WalletCardPreview() {
                 modifier = Modifier
                     .padding(16.dp)
                     .width(500.dp),
+            )
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun WalletCardSelectedPreview() {
+    CsTheme {
+        Surface {
+            WalletCard(
+                userWallet = UserWallet(
+                    id = "",
+                    title = "Debit",
+                    initialBalance = BigDecimal(1499.99),
+                    currency = getUsdCurrency(),
+                    currentBalance = BigDecimal(2499.99),
+                    isPrimary = true,
+                ),
+                expenses = BigDecimal(200),
+                income = BigDecimal(800),
+                onWalletClick = {},
+                onNewTransactionClick = {},
+                onTransferClick = { _ -> },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .width(500.dp),
+                selected = true,
             )
         }
     }
