8219b494447a4ef7ec4d2b2ede70e9a65d3b94e3
Nikita Bulavin <nikbulavin@proton.me>
2025-11-06 11:38:48 +0300

    Style `TotalBalanceCard`
    Replace `OutlinedCard` with a filled `Card` and apply a drop shadow to elevate the `TotalBalanceCard`.

--- feature/home/src/main/kotlin/ru/resodostudios/cashsense/feature/home/HomeScreen.kt
+++ feature/home/src/main/kotlin/ru/resodostudios/cashsense/feature/home/HomeScreen.kt
@@ -28,7 +28,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.dropShadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.shadow.Shadow
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -164,6 +166,14 @@ internal fun HomeScreen(
                             .zIndex(1f)
                             .padding(innerPadding)
                             .padding(horizontal = 16.dp)
+                            .dropShadow(
+                                shape = MaterialTheme.shapes.extraLarge,
+                                shadow = Shadow(
+                                    radius = 8.dp,
+                                    spread = 2.dp,
+                                    color = MaterialTheme.colorScheme.inverseSurface.copy(0.2f),
+                                )
+                            )
                             .clip(MaterialTheme.shapes.extraLarge)
                             .hazeEffect(hazeState, hazeStyle) {
                                 blurEnabled = true
--- feature/home/src/main/kotlin/ru/resodostudios/cashsense/feature/home/TotalBalanceCard.kt
+++ feature/home/src/main/kotlin/ru/resodostudios/cashsense/feature/home/TotalBalanceCard.kt
@@ -1,28 +1,35 @@
 package ru.resodostudios.cashsense.feature.home
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.resodostudios.cashsense.core.designsystem.component.CsListItem
 import ru.resodostudios.cashsense.core.designsystem.icon.CsIcons
 import ru.resodostudios.cashsense.core.designsystem.icon.outlined.AccountBalance
+import ru.resodostudios.cashsense.core.designsystem.theme.CsTheme
 import ru.resodostudios.cashsense.core.locales.R as localesR
 
 @Composable
 internal fun TotalBalanceCard(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = {},
+    shape: Shape = MaterialTheme.shapes.extraLarge,
     headlineContent: @Composable () -> Unit,
 ) {
-    OutlinedCard(
-        shape = MaterialTheme.shapes.extraLarge,
+    Card(
+        shape = shape,
         modifier = modifier,
         onClick = onClick,
         colors = CardDefaults.cardColors().copy(
@@ -45,5 +52,20 @@ internal fun TotalBalanceCard(
                 )
             },
         )
+    }
+}
+
+@Preview
+@Composable
+private fun TotalBalanceCardPreview() {
+    CsTheme {
+        Surface {
+            TotalBalanceCard(
+                headlineContent = {
+                    Text(text = "$2,100")
+                },
+                modifier = Modifier.padding(16.dp),
+            )
+        }
     }
 }
