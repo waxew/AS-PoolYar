9e6fa972e553529d1c769c09fd4c917007c008fd
Nikita Bulavin <nikbulavin@proton.me>
2025-11-15 11:55:59 +0300

    Refactor `applyTransactionFilter` in `TransactionExtensions`

--- core/ui/src/main/kotlin/ru/resodostudios/cashsense/core/ui/util/TransactionExtensions.kt
+++ core/ui/src/main/kotlin/ru/resodostudios/cashsense/core/ui/util/TransactionExtensions.kt
@@ -14,22 +14,20 @@ import ru.resodostudios.cashsense.core.model.data.Transaction
 import ru.resodostudios.cashsense.core.model.data.TransactionFilter
 
 fun List<Transaction>.applyTransactionFilter(transactionFilter: TransactionFilter): FilterableTransactions {
-    val filteredTransactions = this
-        .filter {
-            when (transactionFilter.financeType) {
-                NOT_SET -> true
-                EXPENSES -> it.amount.signum() < 0
-                INCOME -> it.amount.signum() > 0
-            }
+    val filteredTransactions = this.filter { transaction ->
+        val financeTypeMatches = when (transactionFilter.financeType) {
+            NOT_SET -> true
+            EXPENSES -> transaction.amount.signum() < 0
+            INCOME -> transaction.amount.signum() > 0
         }
-        .filter {
-            when (transactionFilter.dateType) {
-                ALL -> true
-                WEEK -> it.timestamp.getZonedWeek() == getCurrentWeek()
-                MONTH -> matchesCurrentMonth(it, transactionFilter.selectedDate)
-                YEAR -> it.timestamp.getZonedYear() == transactionFilter.selectedDate.year
-            }
+        val dateTypeMatches = when (transactionFilter.dateType) {
+            ALL -> true
+            WEEK -> transaction.timestamp.getZonedWeek() == getCurrentWeek()
+            MONTH -> matchesCurrentMonth(transaction, transactionFilter.selectedDate)
+            YEAR -> transaction.timestamp.getZonedYear() == transactionFilter.selectedDate.year
         }
+        financeTypeMatches && dateTypeMatches
+    }
 
     val availableCategories = filteredTransactions
         .asSequence()
@@ -58,6 +56,6 @@ private fun matchesCurrentMonth(
     transaction: Transaction,
     selectedDate: LocalDate,
 ): Boolean {
-    return transaction.timestamp.getZonedYear() == selectedDate.year
-            && transaction.timestamp.getZonedMonth() == selectedDate.month.number
+    return transaction.timestamp.getZonedYear() == selectedDate.year &&
+            transaction.timestamp.getZonedMonth() == selectedDate.month.number
 }
