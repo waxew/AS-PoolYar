24c370b73ef5ef0ffc01bbe852153f917af3f996
Nikita Bulavin <nikbulavin@proton.me>
2026-04-19 08:31:37 +0300

    Update to `targetSdk` `37`

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -18,8 +18,8 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
 
             extensions.configure<LibraryExtension> {
                 configureKotlinAndroid(this)
-                testOptions.targetSdk = 36
-                lint.targetSdk = 36
+                testOptions.targetSdk = 37
+                lint.targetSdk = 37
                 defaultConfig.testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
                 testOptions.animationsDisabled = true
                 resourcePrefix = path.split("""\W""".toRegex())
