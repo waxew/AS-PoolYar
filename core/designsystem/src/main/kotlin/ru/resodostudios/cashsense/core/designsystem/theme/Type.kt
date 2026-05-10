402a693564f7c7d5058637cf9a0a10c4039ba149
Nikita Bulavin <nikbulavin@proton.me>
2026-05-10 10:52:02 +0300

    Update font family to Noto Serif and simplify Typography
    - Replace Roboto Serif with Noto Serif font family and update corresponding font resources.
    - Remove `letterSpacing` and `lineHeightStyle` from `TextStyle` definitions in `Type.kt`.
    - Clean up unused imports related to `LineHeightStyle`.

--- core/designsystem/src/main/kotlin/ru/resodostudios/cashsense/core/designsystem/theme/Type.kt
+++ core/designsystem/src/main/kotlin/ru/resodostudios/cashsense/core/designsystem/theme/Type.kt
@@ -5,15 +5,12 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.text.style.LineHeightStyle.Alignment
-import androidx.compose.ui.text.style.LineHeightStyle.Trim
 import androidx.compose.ui.unit.sp
 import ru.resodostudios.cashsense.core.designsystem.R
 
-internal val robotoSerifFontFamily = FontFamily(
-    Font(resId = R.font.roboto_serif_regular),
-    Font(resId = R.font.roboto_serif_medium, weight = FontWeight.Medium),
+internal val notoSerifFontFamily = FontFamily(
+    Font(resId = R.font.noto_serif_regular),
+    Font(resId = R.font.noto_serif_medium, weight = FontWeight.Medium),
 )
 internal val googleSansFontFamily = FontFamily(
     Font(resId = R.font.google_sans_regular),
@@ -25,113 +22,90 @@ internal val csTypography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 57.sp,
         lineHeight = 64.sp,
-        letterSpacing = (-0.25).sp,
-        fontFamily = robotoSerifFontFamily,
+        fontFamily = notoSerifFontFamily,
     ),
     displayMedium = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 45.sp,
         lineHeight = 52.sp,
-        letterSpacing = 0.sp,
-        fontFamily = robotoSerifFontFamily,
+        fontFamily = notoSerifFontFamily,
     ),
     displaySmall = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 36.sp,
         lineHeight = 44.sp,
-        letterSpacing = 0.sp,
-        fontFamily = robotoSerifFontFamily,
+        fontFamily = notoSerifFontFamily,
     ),
     headlineLarge = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 32.sp,
         lineHeight = 40.sp,
-        letterSpacing = 0.sp,
-        fontFamily = robotoSerifFontFamily,
+        fontFamily = notoSerifFontFamily,
     ),
     headlineMedium = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 28.sp,
         lineHeight = 36.sp,
-        letterSpacing = 0.sp,
-        fontFamily = robotoSerifFontFamily,
+        fontFamily = notoSerifFontFamily,
     ),
     headlineSmall = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 24.sp,
         lineHeight = 32.sp,
-        letterSpacing = 0.sp,
-        fontFamily = robotoSerifFontFamily,
+        fontFamily = notoSerifFontFamily,
     ),
     titleLarge = TextStyle(
         fontWeight = FontWeight.Medium,
         fontSize = 22.sp,
         lineHeight = 28.sp,
-        letterSpacing = 0.sp,
-        fontFamily = robotoSerifFontFamily,
-        lineHeightStyle = LineHeightStyle(
-            alignment = Alignment.Center,
-            trim = Trim.None,
-        ),
+        fontFamily = notoSerifFontFamily,
     ),
     titleMedium = TextStyle(
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.15.sp,
-        fontFamily = robotoSerifFontFamily,
-        lineHeightStyle = LineHeightStyle(
-            alignment = Alignment.Center,
-            trim = Trim.None,
-        ),
+        fontFamily = notoSerifFontFamily,
     ),
     titleSmall = TextStyle(
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        letterSpacing = 0.1.sp,
-        fontFamily = robotoSerifFontFamily,
+        fontFamily = notoSerifFontFamily,
     ),
     bodyLarge = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp,
         fontFamily = googleSansFontFamily,
     ),
     bodyMedium = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        letterSpacing = 0.25.sp,
         fontFamily = googleSansFontFamily,
     ),
     bodySmall = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.4.sp,
         fontFamily = googleSansFontFamily,
     ),
     labelLarge = TextStyle(
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        letterSpacing = 0.1.sp,
         fontFamily = googleSansFontFamily,
     ),
     labelMedium = TextStyle(
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp,
         fontFamily = googleSansFontFamily,
     ),
     labelSmall = TextStyle(
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp,
         fontFamily = googleSansFontFamily,
     )
 )
