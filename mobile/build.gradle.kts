33418b12a4a2490dc71c815e670f7067b9d06c04
Nikita Bulavin <nikbulavin@proton.me>
2026-06-18 23:49:06 +0300

    Revert "Add locale_config.xml and set manifest localeConfig"
    This reverts commit 1961a71e3567331bb8ed6b31458577783cded656.

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -53,6 +53,9 @@ android {
             )
         }
     }
+    androidResources {
+        generateLocaleConfig = true
+    }
     packaging {
         resources {
             excludes.add("/META-INF/{AL2.0,LGPL2.1}")
